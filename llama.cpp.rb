f98b49849a0781b78229dbc4f2e5fb6abf22f6b9
Nicolas Patry <patry.nicolas@protonmail.com>
2024-03-18 14:37:33 +0100

    Prebuilt.

--- llama.cpp.rb
+++ llama.cpp.rb
@@ -3,7 +3,16 @@ $version = "b2453"
 class LlamaCpp < Formula
  desc "Inference of Meta's LLaMA model in pure C/C++"
  homepage "https://github.com/Narsil/llama.cpp"
- url "https://github.com/Narsil/llama.cpp/releases/download/prebuild_metal-b2463-caf0b0b/llama-prebuild_metal-b1-caf0b0b-bin-macos-x64.zip"
+
+
+ on_arm do
+   arch = "arm64"
+   url "https://github.com/Narsil/llama.cpp/releases/download/prebuild_metal-b2465-2f34f73/llama-prebuild_metal-b1-2f34f73-bin-macos-#{arch}.zip"
+ end
+ on_intel do
+   arch = "x64"
+   url "https://github.com/Narsil/llama.cpp/releases/download/prebuild_metal-b2465-2f34f73/llama-prebuild_metal-b1-2f34f73-bin-macos-#{arch}.zip"
+ end
  version "prebuild_metal-b2461-5f45c05"
  sha256 ""
  license "MIT"
@@ -18,5 +27,3 @@ def install
     system "#{bin}/llama", "--version"
  end
 end
-
-
